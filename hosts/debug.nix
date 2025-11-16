b67e49061dbc618a29fe9ba038ecc9520141724d
DarkBones <23454780+DarkBones@users.noreply.github.com>
2025-11-16 18:13:56 +0100

    tmp

--- hosts/debug.nix
+++ hosts/debug.nix
@@ -1,25 +1,24 @@
 { config, lib, pkgs, modulesPath, ... }:
-
 {
-  # Use the same hardware as vm-snowman-test-2
   imports = [ ./vm-snowman-test-2-hardware-configuration.nix ];
 
-  # Simple, explicit GRUB setup on /dev/vda
   boot.loader.grub = {
     enable = true;
     devices = [ "/dev/vda" ];
     useOSProber = false;
   };
 
-  # Minimal networking so you can reach it
   networking.hostName = "debug-minimal";
   networking.useDHCP = true;
 
-  # Root login just to keep it simple for the test
-  users.users.root.initialPassword = "root";
+  # fresh debug user
+  users.users.debug = {
+    isNormalUser = true;
+    initialPassword = "debug";
+    extraGroups = [ "wheel" ]; # so you can sudo
+  };
 
   services.openssh.enable = true;
 
-  # Whatever your release is
   system.stateVersion = "25.05";
 }
