d97423dbd911dfeec5e9d48b5645d83ae12617ce
DarkBones <23454780+DarkBones@users.noreply.github.com>
2025-11-16 16:39:40 +0100

    Cleanup

--- hosts/vm-snowman-test.nix
+++ hosts/vm-snowman-test.nix
@@ -1,9 +1,1 @@
-{ ... }: {
-  imports = [ ./vm-snowman-test-hardware-configuration.nix ];
-
-  boot.loader.grub = {
-    enable = true;
-    devices = [ "/dev/vda" ];
-    useOSProber = false;
-  };
-}
+{ ... }: { }
