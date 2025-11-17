28cc1c8a604c9f24bf16c2160bc2aca96c936678
DarkBones <23454780+DarkBones@users.noreply.github.com>
2025-11-17 19:44:00 +0100

    Reenable hw

--- inventory.nix
+++ inventory.nix
@@ -89,15 +89,15 @@
         "qemu-guest"
       ]; # ONLY for VMs. On normal machines, simply omit this key
 
-      # hardware = {
-      #   boot = { firmware = "none"; }; # "bios" | "efi"
-      #   bootDevice = "/dev/vda";
-      #   fs = {
-      #     type = "ext4";
-      #     partition = 1; # /dev/vda1
-      #     # swapGiB = 0;
-      #   };
-      # };
+      hardware = {
+        boot = { firmware = "none"; }; # "bios" | "efi"
+        bootDevice = "/dev/vda";
+        fs = {
+          type = "ext4";
+          partition = 1; # /dev/vda1
+          # swapGiB = 0;
+        };
+      };
 
       users = [ "bas" ];
 
