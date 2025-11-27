de311d803bd8fd7a8dab89ce8600b3e7ab7d1a36
DarkBones <23454780+DarkBones@users.noreply.github.com>
2025-11-27 19:16:04 +0100

    Bugfixes

--- home/roles/dev-heavy.nix
+++ home/roles/dev-heavy.nix
@@ -1,7 +1,7 @@
 { lib, pkgs, pkgsUnstable, config, ... }:
 let cfg = config.roles.dev;
 in {
-  options.roles.dev.enable = lib.mkEnableOption "Dev role";
+  options.roles.dev-heavy.enable = lib.mkEnableOption "Dev role";
 
   config =
     lib.mkIf cfg.enable { home.packages = with pkgsUnstable; [ starship ]; };
