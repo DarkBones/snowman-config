1a3ebe7b062a774b3e47f5263c104af5dccded03
DarkBones <23454780+DarkBones@users.noreply.github.com>
2025-11-27 18:13:15 +0100

    Optional secrets

--- users/env/bas.nix
+++ users/env/bas.nix
@@ -1,15 +1,21 @@
-{ osConfig, ... }: {
+{ osConfig, lib, ... }: {
   home.sessionPath = [
     "$HOME/.local/state/nix/profiles/home-manager/bin"
     "$HOME/.nix-profile/bin"
   ];
 
-  home.sessionVariables = {
+  home.sessionVariables = let
+    maybe = name:
+      if lib.hasAttr name osConfig.sops.secrets then
+        osConfig.sops.secrets.${name}.path
+      else
+        "";
+  in {
     EDITOR = "nvim";
     LANG = "en_US.UTF-8";
     FLAKE = "~/Developer/snowman";
 
-    TEST_SECRET_PATH = osConfig.sops.secrets.test.path;
-    OPENAI_API_KEY_SECRET_PATH = osConfig.sops.secrets.openai_api_key.path;
+    TEST_SECRET_PATH = maybe "test";
+    OPENAI_API_KEY_SECRET_PATH = maybe "openai_api_key";
   };
 }
