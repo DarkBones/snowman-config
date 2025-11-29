a1d5e7dab0dd104a535bc22193953979360b42b9
DarkBones <23454780+DarkBones@users.noreply.github.com>
2025-11-29 18:32:50 +0100

    Update snowman

--- hosts/rpi4.nix
+++ hosts/rpi4.nix
@@ -1,14 +1,15 @@
-# TODO: Try without this file
-{ pkgs, lib, ... }: {
-  networking.hostName = "rpi4";
-
-  networking.firewall = {
-    enable = true;
-    allowPing = true;
-    allowedTCPPorts = [ 22 ];
-
-    checkReversePath = "loose";
-
-    trustedInterfaces = [ "wlan0" ];
-  };
-}
+# # TODO: Try without this file
+# { pkgs, lib, ... }: {
+#   networking.hostName = "rpi4";
+#
+#   networking.firewall = {
+#     enable = true;
+#     allowPing = true;
+#     allowedTCPPorts = [ 22 ];
+#
+#     checkReversePath = "loose";
+#
+#     trustedInterfaces = [ "wlan0" ];
+#   };
+# }
+{ }
