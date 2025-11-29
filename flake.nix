c0249dd1e2101307795d7dced1718c568b8eef70
DarkBones <23454780+DarkBones@users.noreply.github.com>
2025-11-29 13:00:18 +0100

    Bugfixes

--- flake.nix
+++ flake.nix
@@ -56,6 +56,7 @@
         modulesPath = "${nixpkgs}/nixos/modules";
         currentHost = name;
         sopsConfigPath = ./.sops.yaml;
+        networkSecretsPath = ./networks/secrets.yml;
 
         extraHomeImports = [ ./home/roles ];
       };
