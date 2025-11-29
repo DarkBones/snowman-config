970a193b1ba049e15c0ffe788cfe301da7b7f28c
DarkBones <23454780+DarkBones@users.noreply.github.com>
2025-11-29 14:35:07 +0100

    Bugfixes

--- hosts/rpi4.nix
+++ hosts/rpi4.nix
@@ -1,1 +1,9 @@
-{ pkgs, lib, ... }: { networking.hostName = "rpi4"; }
+{ pkgs, lib, ... }: {
+  networking.hostName = "rpi4";
+  networking.firewall = {
+    enable = true;
+    allowPing = true;
+
+    allowedTCPPorts = [ 22 ];
+  };
+}
