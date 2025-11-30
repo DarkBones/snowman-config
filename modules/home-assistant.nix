010dc6696d45d5daa5b6bd97f8bd2f54bbd299c0
DarkBones <23454780+DarkBones@users.noreply.github.com>
2025-11-30 18:32:41 +0100

    Add home assistant module

--- modules/home-assistant.nix
+++ modules/home-assistant.nix
@@ -4,13 +4,55 @@
     openFirewall = true;
     configDir = "/var/lib/home-assistant";
 
+    defaultIntegrations = [
+      "application_credentials"
+      "frontend"
+      "hardware"
+      "logger"
+      "network"
+      "system_health"
+      "automation"
+      "person"
+      "scene"
+      "script"
+      "tag"
+      "zone"
+      "counter"
+      "input_boolean"
+      "input_button"
+      "input_datetime"
+      "input_number"
+      "input_select"
+      "input_text"
+      "schedule"
+      "timer"
+      "backup"
+    ];
+
     extraPackages = python3Packages:
       with python3Packages; [
         aiohue
         pychromecast
         python-roborock
-        python-otbr-api
         vacuum-map-parser-roborock
+
+        # for thread / otbr
+        python-otbr-api
+
+        # for google_translate TTS
+        gtts
+
+        # for Fritz!Box / Fritz integrations
+        fritzconnection
+        pyfritzhome
+
+        # for UPnP, various discovery things
+        getmac
+        pyipp
+
+        # for BLE integrations
+        govee-ble
+        ibeacon-ble
       ];
 
     config = { default_config = { }; };
