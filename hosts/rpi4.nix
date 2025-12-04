7763a98d1e10828b78b2e8253c4e7befd0726dfd
DarkBones <23454780+DarkBones@users.noreply.github.com>
2025-12-04 17:47:49 +0100

    Working GA Integration

--- hosts/rpi4.nix
+++ hosts/rpi4.nix
@@ -1,15 +1,40 @@
 { pkgs, lib, ... }: {
-  imports = [ ./rpi4-hardware-configuration.nix ];
+  imports = [ 
+    ./rpi4-hardware-configuration.nix 
+    ../modules/home-assistant.nix 
+  ];
 
-  boot.loader.grub.enable = false;
-  boot.loader.generic-extlinux-compatible.enable = true;
+  boot = {
+    loader = {
+      grub.enable = false;
+      generic-extlinux-compatible.enable = true;
+    };
+    # Fixes IPv6 socket crashes
+    kernelParams = [ "ipv6.disable=1" ];
+  };
 
-  networking.firewall = {
-    enable = true;
-    allowPing = true;
-    allowedTCPPorts = [ 22 ];
-    checkReversePath = "loose";
-    trustedInterfaces = [ "wlan0" ];
+  # Fixes Google/SSL Token Expiry issues
+  time.timeZone = "Europe/Berlin";
+
+  networking = {
+    enableIPv6 = false;
+
+    # Fixes "Could not contact DNS servers"
+    nameservers = [ "1.1.1.1" "8.8.8.8" ];
+
+    firewall = {
+      enable = true;
+      allowPing = true;
+      
+      # Open HA Dashboard (8123) and SSH (22)
+      allowedTCPPorts = [ 22 8123 ];
+      
+      # Open mDNS/Cast discovery
+      allowedUDPPorts = [ 5353 ];
+      
+      checkReversePath = "loose";
+      trustedInterfaces = [ "wlan0" ];
+    };
   };
 
   hardware.bluetooth = {
--- modules/home-assistant.nix
+++ modules/home-assistant.nix
@@ -1,36 +1,48 @@
-{ pkgs, lib, ... }: {
+{ pkgs, lib, ... }:
+{
   services.home-assistant = {
     enable = true;
-    openFirewall = true;
     configDir = "/var/lib/home-assistant";
 
+    # Python packages
     extraPackages = python3Packages: [
       python3Packages.aiohue
       python3Packages.pychromecast
       python3Packages.python-roborock
       python3Packages.vacuum-map-parser-roborock
-
-      # for thread / otbr
       python3Packages.python-otbr-api
-
-      # for google_translate TTS
       python3Packages.gtts
-
-      # for Fritz!/UPnP/etc
       python3Packages.fritzconnection
       python3Packages.pyfritzhome
       python3Packages.getmac
       python3Packages.pyipp
-
-      # for BLE integrations
       python3Packages."govee-ble"
       python3Packages."ibeacon-ble"
       python3Packages."kegtron-ble"
+      python3Packages.google-cloud-pubsub
+      python3Packages.google-cloud-speech
+      python3Packages.grpcio
+      python3Packages.grpcio-tools
+      python3Packages."xiaomi-ble"
     ];
 
     config = {
       default_config = { };
+
       automation = "!include automations.yaml";
+      script     = "!include scripts.yaml";
+      scene      = "!include scenes.yaml";
+
+      http = {
+        server_host = [ "0.0.0.0" ];
+        server_port = 8123;
+      };
     };
+  };
+
+  # Systemd override to fix startup race conditions
+  systemd.services.home-assistant = {
+    wants = [ "network-online.target" ];
+    after = [ "network-online.target" ];
   };
 }
