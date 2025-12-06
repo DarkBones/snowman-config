461aa407f305a114f4eae828ec59861a8223c668
DarkBones <23454780+DarkBones@users.noreply.github.com>
2025-12-06 14:05:36 +0100

    tmp

--- home/overrides/dev-dotfiles.nix
+++ home/overrides/dev-dotfiles.nix
@@ -1,4 +1,4 @@
-{ dotfilesSources, ... }:
+{ lib, dotfilesSources, ... }:
 let
   dotfilesProd = dotfilesSources.bas;
   dotfilesDev = "/home/bas/Developer/dotfiles";
@@ -6,8 +6,8 @@ let
   mode = builtins.getEnv "SNOWMAN_DOTFILES_MODE";
   useDev = mode == "dev";
 in {
-  home.file.".config/nvim".source = if useDev then
+  home.file.".config/nvim".source = lib.mkForce (if useDev then
     "${dotfilesDev}/nvim/.config/nvim"
   else
-    "${dotfilesProd}/nvim/.config/nvim";
+    "${dotfilesProd}/nvim/.config/nvim");
 }
