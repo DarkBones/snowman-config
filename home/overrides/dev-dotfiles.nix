6efbbeeb91a7114c4a0dcf08cc50436931e0cdfd
DarkBones <23454780+DarkBones@users.noreply.github.com>
2025-12-07 15:02:59 +0100

    Bugfixes

--- home/overrides/dev-dotfiles.nix
+++ home/overrides/dev-dotfiles.nix
@@ -1,18 +1,15 @@
 { lib, dotfilesSources, ... }:
 let
-  dotfilesProd = dotfilesSources.bas;
-
+  dotfilesProd = dotfilesSources.bas or null;
   mode   = builtins.getEnv "SNOWMAN_DOTFILES_MODE";
   useDev = mode == "dev";
 in
 {
-  # In PROD mode, manage ~/.config/nvim from the Nix store.
-  # In DEV mode, Home Manager leaves ~/.config/nvim alone so you can point it
-  # at ~/Developer/dotfiles manually.
-  home.file = lib.mkIf (!useDev) {
-    ".config/nvim" = lib.mkForce {
-      source    = "${dotfilesProd}/nvim/.config/nvim";
-      recursive = true;
+  config = lib.mkIf (dotfilesProd != null) {
+    home.file = lib.mkIf (!useDev) {
+      ".config/nvim" = lib.mkForce {
+        source = "${dotfilesProd}/nvim/.config/nvim";
+      };
     };
   };
 }
