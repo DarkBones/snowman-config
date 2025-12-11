2371e501d6b7fed27e38ea7d81be4d983d6d8531
DarkBones <23454780+DarkBones@users.noreply.github.com>
2025-12-11 15:31:08 +0100

    tmp

--- hosts/dorkbones.nix
+++ hosts/dorkbones.nix
@@ -1,8 +1,8 @@
 { pkgs, lib, ... }: {
   time.timeZone = "Europe/Berlin";
   hardware = {
-    boot.firmware = "efi";
-    bootDevice = "/dev/nvme0n1";
+    # boot.firmware = "efi";
+    # bootDevice = "/dev/nvme0n1";
     bluetooth = {
       enable = true;
       powerOnBoot = true;
