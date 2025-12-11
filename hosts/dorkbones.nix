de4432a889111af5afe51d89925673f510c01e67
DarkBones <23454780+DarkBones@users.noreply.github.com>
2025-12-11 15:02:32 +0100

    Add bootloader

--- hosts/dorkbones.nix
+++ hosts/dorkbones.nix
@@ -1,7 +1,11 @@
 { pkgs, lib, ... }: {
   time.timeZone = "Europe/Berlin";
-  hardware.bluetooth = {
-    enable = true;
-    powerOnBoot = true;
+  hardware = {
+    boot.firmware = "efi";
+    bootDevice = "/dev/nvme0n1";
+    bluetooth = {
+      enable = true;
+      powerOnBoot = true;
+    };
   };
 }
