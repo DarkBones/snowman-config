20eabc60d919a092e213a7d036d5a7a521eb37c1
DarkBones <23454780+DarkBones@users.noreply.github.com>
2025-12-12 12:22:41 +0100

    Update snowman

--- modules/nvidia.nix
+++ modules/nvidia.nix
@@ -3,6 +3,11 @@
 
   services.xserver.videoDrivers = [ "nvidia" ];
 
+  boot.initrd.kernelModules =
+    [ "nvidia" "nvidia_modeset" "nvidia_uvm" "nvidia_drm" ];
+
+  boot.extraModulePackages = [ config.boot.kernelPackages.nvidiaPackages.beta ];
+
   boot.kernelParams = [ "nvidia_drm.modeset=1" "nvidia_drm.fbdev=1" ];
 
   environment.sessionVariables = {
@@ -10,17 +15,18 @@
     XDG_SESSION_TYPE = "wayland";
     GBM_BACKEND = "nvidia-drm";
     __GLX_VENDOR_LIBRARY_NAME = "nvidia";
-
     WLR_NO_HARDWARE_CURSORS = "1";
   };
 
   hardware.nvidia = {
     modesetting.enable = true;
 
+    # Required for 40-series stability and proper resume/start.
+    powerManagement.enable = true;
+    powerManagement.finegrained = false;
+
     open = false;
-
     package = config.boot.kernelPackages.nvidiaPackages.beta;
-
     nvidiaSettings = true;
   };
 }
