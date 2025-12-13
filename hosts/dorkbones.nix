e052330c5b0ada4010542661cea339bc695c2bdf
DarkBones <23454780+DarkBones@users.noreply.github.com>
2025-12-13 10:47:11 +0100

    Update snowman

--- hosts/dorkbones.nix
+++ hosts/dorkbones.nix
@@ -1,6 +1,7 @@
 { pkgs, lib, ... }: {
   time.timeZone = "Europe/Berlin";
   i18n.defaultLocale = "en_US.UTF-8";
+  boot.initrd.systemd.enable = true;
   hardware = {
     # bootDevice = "/dev/nvme0n1";
     bluetooth = {
