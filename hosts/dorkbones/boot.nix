6b5bc19922fd607dae8a434b0fe9a765bc03128b
DarkBones <23454780+DarkBones@users.noreply.github.com>
2025-12-14 07:54:23 +0100

    Update snowman

--- hosts/dorkbones/boot.nix
+++ hosts/dorkbones/boot.nix
@@ -2,8 +2,4 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.efi.efiSysMountPoint = "/boot";
-  # fileSystems."/boot" = {
-  #   device = "/dev/disk/by-partuuid/a1abf22f-30d2-44f6-ab70-142f6951e307";
-  #   fsType = "vfat";
-  # };
 }
