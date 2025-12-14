c47768419c9786bca7dc746af34570e08cc94a9d
DarkBones <23454780+DarkBones@users.noreply.github.com>
2025-12-14 14:43:05 +0100

    Add tailscale

--- hosts/dorkbones.nix
+++ hosts/dorkbones.nix
@@ -2,11 +2,36 @@
   time.timeZone = "Europe/Berlin";
   i18n.defaultLocale = "en_US.UTF-8";
   boot.initrd.systemd.enable = true;
-  hardware = {
-    # bootDevice = "/dev/nvme0n1";
-    bluetooth = {
+
+  networking = {
+    enableIPv6 = false;
+
+    nameservers = [ "1.1.1.1" "8.8.8.8" ]; # TODO: Quad 9
+
+    firewall = {
       enable = true;
-      powerOnBoot = true;
+      allowPing = true;
+
+      # LAN access: SSH + Home Assistant
+      allowedTCPPorts = [ 22 8123 ];
+
+      # LAN discovery
+      allowedUDPPorts = [ 5353 ];
+
+      checkReversePath = "loose";
+
+      # Trust LAN + Tailscale interfaces
+      trustedInterfaces = [ "wlan0" "tailscale0" ];
     };
+  };
+
+  services.tailscale = {
+    enable = true;
+    openFirewall = true;
+  };
+
+  hardware.bluetooth = {
+    enable = true;
+    powerOnBoot = true;
   };
 }
