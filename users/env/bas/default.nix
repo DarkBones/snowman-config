512d8e58102fc477926b088fe84acd7c06c31b80
DarkBones <23454780+DarkBones@users.noreply.github.com>
2026-02-01 12:13:33 +0100

    Fix secrets

--- users/env/bas/default.nix
+++ users/env/bas/default.nix
@@ -1,26 +1,23 @@
-{ osConfig, lib, ... }: {
-  home.sessionPath = [
-    "$HOME/.local/state/nix/profiles/home-manager/bin"
-    "$HOME/.nix-profile/bin"
-    "$HOME/.npm-global/bin"
-    "$HOME/bin"
-    "$HOME/.local/bin"
-  ];
+{ osConfig, lib, ... }:
+let
+  maybe = name:
+    if lib.hasAttr name osConfig.sops.secrets then
+      osConfig.sops.secrets.${name}.path
+    else
+      "";
 
-  home.sessionVariables = let
-    maybe = name:
-      if lib.hasAttr name osConfig.sops.secrets then
-        osConfig.sops.secrets.${name}.path
-      else
-        "";
-  in {
+  vars = {
     EDITOR = "nvim";
     LANG = "en_US.UTF-8";
+
     FLAKE = "${osConfig.users.users.bas.home}/Developer/snowman";
     SNOWMAN_FLAKE = "${osConfig.users.users.bas.home}/snowman-config";
 
     OPENAI_API_KEY_SECRET_PATH = maybe "openai_api_key";
     GEMINI_API_KEY_SECRET_PATH = maybe "gemini_api_key";
-    MAIN_KEY_SECRET_PATH = maybe "darkbones_dev_key";
   };
+in {
+  home.sessionVariables = vars;
+
+  systemd.user.sessionVariables = vars;
 }
