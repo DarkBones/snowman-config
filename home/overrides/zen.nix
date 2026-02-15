09eaf3ab6cb5cc38b61dbe936bded4510e0f7a32
DarkBones <23454780+DarkBones@users.noreply.github.com>
2026-02-15 09:11:56 +0100

    Cleanup

--- home/overrides/zen.nix
+++ home/overrides/zen.nix
@@ -1,5 +1,7 @@
-{ lib, pkgs, config, ... }: {
-  config = lib.mkIf pkgs.stdenv.isLinux {
+{ lib, pkgs, config, ... }:
+let dotfilesEnabled = (config.roles.dotfiles.enable or false);
+in {
+  config = lib.mkIf (pkgs.stdenv.isLinux && !dotfilesEnabled) {
     xdg.configFile."gtk/darkling.css".source =
       config.lib.file.mkOutOfStoreSymlink
       "${config.dotfiles.root}/gtk/.config/gtk/darkling.css";
