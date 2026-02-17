0cdc0b606e7e4bbe79de65413a9dbf4e8b899b0e
DarkBones <23454780+DarkBones@users.noreply.github.com>
2026-02-17 17:33:53 +0100

    Get taskd working

--- modules/taskserver.nix
+++ modules/taskserver.nix
@@ -1,10 +1,22 @@
-{ ... }: {
+{ pkgs, ... }:
+let
+  taskdWrapped = pkgs.writeShellScriptBin "taskd" ''
+    set -euo pipefail
+    if [ "$#" -lt 1 ]; then
+      exec ${pkgs.taskserver}/bin/taskd --help
+    fi
+    cmd="$1"
+    shift
+    exec ${pkgs.taskserver}/bin/taskd "$cmd" --data /var/lib/taskserver "$@"
+  '';
+in {
   services.taskserver = {
     enable = true;
     dataDir = "/var/lib/taskserver";
-
     config = { "server.listen" = "0.0.0.0:53589"; };
   };
+
+  environment.systemPackages = [ taskdWrapped ];
 
   networking.firewall.allowedTCPPorts = [ 53589 ];
 }
