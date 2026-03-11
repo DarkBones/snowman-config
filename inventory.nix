7d8a9e52cb282a5555ba4adb9340f93d189e611f
DarkBones <23454780+DarkBones@users.noreply.github.com>
2026-03-11 08:56:19 +0100

    Remove raycast

--- inventory.nix
+++ inventory.nix
@@ -141,7 +141,6 @@ rec {
             ".config/karabiner" = "karabiner/.config/karabiner";
             ".config/MangoHud" = "mangohud/.config/MangoHud";
             ".config/nvim" = "nvim/.config/nvim";
-            ".config/raycast" = "raycast/.config/raycast";
             ".config/skhd" = "skhd/.config/skhd";
             ".config/starship.toml" = "starship/.config/starship.toml";
             ".config/swaync" = "swaync/.config/swaync";
