8ea5d9bb40fb0a57bcfdef19f9a2c4ee7ba4aee1
DarkBones <23454780+DarkBones@users.noreply.github.com>
2026-03-13 16:15:32 +0100

    Update ollama

--- modules/openwebui.nix
+++ modules/openwebui.nix
@@ -1,7 +1,11 @@
-{ lib, pkgs, ... }: {
+{ ... }: {
   services.open-webui = {
     enable = true;
     host = "127.0.0.1";
     port = 4080;
+    environment = {
+      HOME = "/var/lib/open-webui";
+      NLTK_DATA = "/var/lib/open-webui/nltk_data";
+    };
   };
 }
