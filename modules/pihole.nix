ecabebb080510c25d5ddafcf29a8ceb516914392
DarkBones <23454780+DarkBones@users.noreply.github.com>
2026-03-19 17:58:23 +0100

    Add upstream resolvers

--- modules/pihole.nix
+++ modules/pihole.nix
@@ -9,7 +9,8 @@
 
     settings = {
       dns = {
-        upstreams = [ "1.1.1.1" "1.0.0.1" ];
+        upstreams =
+          [ "1.1.1.1" "1.0.0.1" "2606:4700:4700::1111" "2606:4700:4700::1001" ];
         listeningMode = "LOCAL";
         interface = "end0";
       };
