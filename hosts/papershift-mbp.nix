2d08eb9563376a3504dd8f0d5bd1631d80b61ce3
DarkBones <23454780+DarkBones@users.noreply.github.com>
2026-03-21 10:07:47 +0100

    Create host and module

--- hosts/papershift-mbp.nix
+++ hosts/papershift-mbp.nix
@@ -1,16 +1,21 @@
-{ pkgs, lib, ... }:
-let papershiftRoot = "/Users/bas/Developer/papershift";
+{ pkgs, lib, config, ... }:
+let
+  homeDir =
+    config.home-manager.users.bas.home.homeDirectory or (if pkgs.stdenv.isDarwin then
+      "/Users/bas"
+    else
+      "/home/bas");
+
+  papershiftRoot = "${homeDir}/Developer/papershift";
 in {
-  home-manager.users.bas = {
-    home.packages = [
-      (pkgs.writeShellScriptBin "pulse-shell" ''
-        cd "${papershiftRoot}/pulse"
-        exec ${pkgs.bashInteractive}/bin/bash
-      '')
-      (pkgs.writeShellScriptBin "core-shell" ''
-        cd "${papershiftRoot}/core"
-        exec ${pkgs.bashInteractive}/bin/bash
-      '')
-    ];
-  };
+  home-manager.users.bas.home.packages = [
+    (pkgs.writeShellScriptBin "pulse-shell" ''
+      cd "${papershiftRoot}/pulse"
+      exec ${pkgs.bashInteractive}/bin/bash
+    '')
+    (pkgs.writeShellScriptBin "core-shell" ''
+      cd "${papershiftRoot}/core"
+      exec ${pkgs.bashInteractive}/bin/bash
+    '')
+  ];
 }
