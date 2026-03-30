b418696ab70301ffad862ac499d73dc7d16b267b
DarkBones <23454780+DarkBones@users.noreply.github.com>
2026-03-30 19:40:51 +0200

    Progress

--- modules/alvr.nix
+++ modules/alvr.nix
@@ -1,11 +1,26 @@
-{ lib, pkgs, config, ... }:
+{ lib, pkgs, pkgsUnstable, config, ... }:
 let cfg = config.roles.alvr;
+    alvrPkg = pkgsUnstable.alvr.overrideAttrs (old: {
+      postInstall = (old.postInstall or "") + ''
+        mv $out/libexec/alvr/vrcompositor-wrapper $out/libexec/alvr/vrcompositor-wrapper-unwrapped
+        cat > $out/libexec/alvr/vrcompositor-wrapper << EOF
+#!/usr/bin/env bash
+unset LD_LIBRARY_PATH
+unset LD_PRELOAD
+unset VRCOMPOSITOR_LD_LIBRARY_PATH
+unset STEAM_RUNTIME
+unset STEAM_ZENITY
+exec ${pkgs.steam-run}/bin/steam-run $out/libexec/alvr/vrcompositor-wrapper-unwrapped "\$@"
+EOF
+        chmod +x $out/libexec/alvr/vrcompositor-wrapper
+      '';
+    });
 in {
   options.roles.alvr.enable = lib.mkEnableOption "ALVR VR streaming";
 
   config = lib.mkIf cfg.enable {
     environment.systemPackages = with pkgs; [
-      alvr
+      alvrPkg
     ];
 
     # ADB for Quest connection (includes udev rules)
